e0be85196ac049fecc8ef1efd4470a87acf55539
ferretdayo <ferretdayo@hotmail.co.jp>
2017-02-19 19:16:27 +0900

    [WIP]tagの追加はできた

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -18,13 +18,36 @@ class TagsController < ApplicationController
 
     # タグの追加
     def create
+        @user = User.find(params[:id])
         @tag = Tag.find_by(tag: params[:tag])
         # タグがあった場合、対象のtagのidから設定
         if @tag
+            # tag_idとuser_idの組み合わせがある場合は追加しない
+            @tagUser = TagUser.find_by(tag_id: @tag.id, user_id: params[:id])
+            if @tagUser
+                render json: {'msg': "already exist", 'status': false}
+            else 
+                # タグとユーザの関連の追加
+                @newTagUser = TagUser.create({tag_id: @tag.id, user_id: params[:id]})
+                if @newTagUser
+                    render json: {'msg': "success to add tag", 'status': true}
+                else
+                    render json: {'msg': "failed to add tag", 'status': false}
+                end
+            end
         else
-            newTag = Tag.create({tag: params[:tag]});
-        end
+            # TODO transactionですべき・・・
+            
+            # tagがない場合は新規に作成
+            @newTag = Tag.create({tag: params[:tag]});
+            @newTagUser = TagUser.create({tag_id: @newTag.id, user_id: params[:id]})
 
+            if @newTag && @newTagUser
+                render json: {'msg': "success to add tag", 'status': true}
+            else
+                render json: {'msg': "failed to add tag", 'status': false}
+            end
+        end
     end
 
     def update
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,12 @@
 ActiveRecord::Schema.define(version: 20170219080100) do
 
   create_table "tag_users", force: :cascade do |t|
-    t.integer  "user_id_id"
-    t.integer  "tag_id_id"
+    t.integer  "user_id"
+    t.integer  "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["tag_id_id"], name: "index_tag_users_on_tag_id_id"
-    t.index ["user_id_id"], name: "index_tag_users_on_user_id_id"
+    t.index ["tag_id"], name: "index_tag_users_on_tag_id"
+    t.index ["user_id"], name: "index_tag_users_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
