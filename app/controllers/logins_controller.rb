4ecff4f8cdcd3949fd28362cff75f512fd3b2c24
ferretdayo <ferretdayo@hotmail.co.jp>
2017-03-04 13:42:17 +0900

    リファクタリングをした

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,40 +1,21 @@
 class LoginsController < ApplicationController
-    use ActionDispatch::Session::CookieStore
-    def index
-
-    end
-
-    def new
-
-    end
-
-    def show
-
-    end
-
-    def edit
-
-    end
-
     # ログイン
     def create
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
             log_in user
             render :json => {'msg': 'success login', 'status': true, 'current_user': current_user }
+            return
         else
             render :json => {'msg': 'failed login', 'status': false}
+            return
         end
     end
-
-    def update
-
-    end
-
     # ログアウト
     def destroy
         session[:user_id] = nil
         @current_user = nil
         render :json => {'msg': 'success logout', 'status': true}
+        return
     end
 end
